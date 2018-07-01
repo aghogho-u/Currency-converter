7eff6e41a7f9c2f61f4d44f0e9988f96c2b7b835
AGHOGHO UWEJEYAN <auwejeyan@gmail.com>
2018-07-01 14:45:21 +0100

    dropdowns populated onload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function getConversionRates() {
+ getConversionRates = () => {
   let labelVal = document.getElementById("result");
-  labelVal.innerHTML = "Current rate is: ";
+      labelVal.innerHTML = "Current rate is: ";
   let currAmt = parseFloat(document.getElementById("amount").value);
   let currFrSelect = document.getElementById("currencyFrom").value;
 
@@ -9,30 +9,28 @@ function getConversionRates() {
   let currPair = `${currFrSelect}_${currToSelect}`;
   let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${currPair}&compact=ultra`;
 
-  function logResult(data) {
+  logResult= (data) => {
     let result = parseFloat(
-      Math.round(data[currPair] * currAmt)
+      Math.round(data[currPair] * currAmt*100)/100
     ).toLocaleString();
     console.log(data); 
     labelVal.innerHTML += result;
   }
 
-  function logError(error) {
-    console.log("Fetching the currency pair was a problem: \n", error);
-  }
+  logError= (error) => console.log("Fetching the currency pair was a problem: \n", error);
+  
 
-  function validateResponse(response) {
+  validateResponse = (response) => {
     if (!response.ok) {
       throw Error(response.statusText);
     }
     return response;
   }
 
-  function readResponseAsJSON(response) {
-    return response.json();
-  }
+  readResponseAsJSON = (response) =>  {return response.json();}
+  
 
-  function fetchJSON(x) {
+  fetchJSON= (x) => {
     fetch(x)
       .then(validateResponse)
       .then(readResponseAsJSON)
@@ -45,43 +43,34 @@ function getConversionRates() {
 
 
 
-function testOnload() {
+fetchOnload = () => {
   let currencies = "https://free.currencyconverterapi.com/api/v5/currencies?";
 
-  function fillSelect(data) {
+  fillSelect =(data)=> {
     let myObject = data.results;
-    // myArr = [];
     let select = document.getElementById("currencyFrom");
     let select2 = document.getElementById("currencyTo");
     for (let key in myObject) {
-      // myArr.push(myObject[key].id);
-      //}
-      //console.log(myArr);
-      //       for(index of myobject) {
       select.options[select.options.length] = new Option(myObject[key].id, key);
-      select2.options[select2.options.length] = new Option(
-        myObject[key].id,
-        key
-      );
+      select2.options[select2.options.length] = new Option(myObject[key].id,key);
     }
   }
 
-  function logError(error) {
-    console.log("Getting the list of currencies was a problem: \n", error);
-  }
+  logError=(error)=> console.log("Getting the list of currencies was a problem: \n", error);
+  
 
-  function validateResponse(response) {
+  validateResponse=(response)=> {
     if (!response.ok) {
       throw Error(response.statusText);
     }
     return response;
   }
 
-  function readResponseAsJSON(response) {
+  readResponseAsJSON=(response)=> {
     return response.json();
   }
 
-  function fetchJSON2(x) {
+  fetchJSON2=(x)=> {
     fetch(x)
       .then(validateResponse)
       .then(readResponseAsJSON)
